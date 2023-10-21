26fab6aa22c34aa227c1f67abb4fd48d83deec3a
nandangp <gpnandan443@gmail.com>
2023-10-21 23:37:05 +0530

    deployment

--- wheather/wheather/settings.py
+++ wheather/wheather/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = "django-insecure-z2iayf^c@h!f7^v5%@j-r6nc!knxta3mgbar-)#fynrhc^g0df
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
 
 # Application definition
 
@@ -74,14 +73,22 @@ WSGI_APPLICATION = "wheather.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+} """
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'feChXLWVqljLCx5tkUbo',
+        'HOST': 'containers-us-west-209.railway.app',
+        'PORT': '6174',
+    }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
@@ -122,3 +129,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
